a870b62764327f0fe0bcb195a94fcd2219e04130
AlexB95 <45.al3x@gmail.com>
2018-03-16 16:41:02 -0600

    Fixed background bug

--- src/pages/preview/preview.ts
+++ src/pages/preview/preview.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Song } from '../../models/song/song.interface';
 import { Media, MediaObject } from '@ionic-native/media';
 import { ProgressBarComponent } from '../../components/progress-bar/progress-bar';
+import { Platform } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -16,63 +17,78 @@ export class PreviewPage {
   loadProgress: number = 0;
   time: number = 0;
   timeSet: any;
-constructor(
-  private navCtrl: NavController,
-  private navParams: NavParams,
-  private media: Media) {
+  flag: boolean = false;
+  constructor(
+    private navCtrl: NavController,
+    private navParams: NavParams,
+    private media: Media,
+    private platform: Platform) {
+    
+  }
 
-}
+  file: MediaObject;
 
-file: MediaObject;
+  ionViewWillLoad() {
+    this.platform.ready().then(() => {
+      this.platform.pause.subscribe(() => {
+        this.pauseSong();
+      });
+    });
+    this.song = this.navParams.get('song');
+  }
 
-ionViewWillLoad() {
-  this.song = this.navParams.get('song');
-}
+  ionViewWillLeave() {
+    this.file.release();
+  }
 
-ionViewWillLeave() {
-  this.file.release();
-}
+  playSong(url) {
+    this.file = this.media.create(url);
+    this.file.play();
+    this.flag = true;
+    this.timeSet = this.timeInterval();
+  }
 
-playSong(url) {
-  this.file = this.media.create(url);
-  this.file.play();
-  this.timeSet = this.timeInterval();
-}
-
-timeInterval() {
-  return setInterval(() => {
-    if (this.time < 30) {
-      this.time++;
-      this.loadProgress = (this.time / 30) * 100;
-    } else {
-      clearInterval(this.timeSet);
-      this.time == 0;
-      this.loadProgress = 0;
-      this.file.release();
-      this.playPause = "md-play";
-    }
-  }, 1000);
-}
-
-pauseSong() {
-  this.file.pause();
-}
-
-playPauseSong() {
-  if (this.playPause === "md-play") {
-    this.playPause = "md-pause";
-    if (!this.file) {
-      this.playSong(this.song.preview_url);
-    } else {
-      this.file.play();
-      this.timeSet = this.timeInterval();
-    }
-  } else {
+  pauseSong() {
     this.playPause = "md-play";
-    if (this.file) {
-      this.pauseSong();
-      clearInterval(this.timeSet);
+    this.file.pause();
+    this.flag = false;
+    clearInterval(this.timeSet);
+  }
+
+  timeInterval() {
+    return setInterval(() => {
+      if (this.time < 30) {
+        if (!this.flag) {
+          this.file.play();
+          this.flag = true;
+        }
+        this.time++;
+        this.loadProgress = (this.time / 30) * 100;
+      } else {
+        this.time = 0;
+        this.flag = false;
+        this.loadProgress = 0;
+        this.playPause = "md-play";
+        this.file.release();
+        this.file = null;
+        clearInterval(this.timeSet);
+      }
+    }, 1000);
+  }
+
+  playPauseSong() {
+    if (this.playPause === "md-play") {
+      this.playPause = "md-pause";
+      if (!this.file) {
+        this.playSong(this.song.preview_url);
+      } else {
+        this.timeSet = this.timeInterval();
+      }
+    } else {
+      this.playPause = "md-play";
+      if (this.file) {
+        this.pauseSong();
+      }
     }
   }
-}
 }
